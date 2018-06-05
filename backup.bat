6d79a07ea40fa8b0665fa00616efdac6de994339
cah4e3 <cah4e3@gmail.com>
2018-06-05 17:11:57 +0300

    apu scripts converted

--- backup.bat
+++ backup.bat
@@ -3,6 +3,7 @@ mkdir .\backup\current\nas
 mkdir .\backup\current\chr
 mkdir .\backup\current\docs
 mkdir .\backup\current\tlm
+mkdir .\backup\current\mus
 mkdir .\backup\current\res
 mkdir .\backup\current\res\tools
 
@@ -13,6 +14,7 @@ copy /b nas\*.nas .\backup\current\nas\*.nas
 copy /b chr\*.chr .\backup\current\chr\*.chr
 copy /b docs\*.txt .\backup\current\docs\*.txt
 copy /b tlm\*.* .\backup\current\tlm\*.*
+copy /b mus\*.* .\backup\current\mus\*.*
 copy /b res\*.* .\backup\current\res\*.*
 copy /b res\tools\*.* .\backup\current\res\tools\*.*
 copy /b *.txt .\backup\current\*.txt
